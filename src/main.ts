f8a0926625adff022048b787ec3278079542f70e
barbarahellmann <babshellmann@web.de>
2024-04-15 16:02:51 +0200

    Typo fix

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-// Step 1: Create an array of numbers. Use the '.map' function to double all the numbers in the array.
+// Step 1:
+// Create an array of numbers. Use the '.map' function to double all the numbers in the array.
 
 const simpleNumbers: number[] = [5,10,15,20,25]
 
@@ -7,16 +8,23 @@ const doubleNumbers: number[] = simpleNumbers.map(((number: number) => number * 
 console.log("Einfache Zahlenfolge: " + simpleNumbers)
 console.log("Jede Zahl verdoppelt: " + doubleNumbers)
 
-// Step 2: Create an array of words. Use the '.filter' function to select only the words that are longer than 5 letters.
+
+
+
+// Step 2:
+// Create an array of words. Use the '.filter' function to select only the words that are longer than 5 letters.
 const arrayOfWords: String[] = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen"]
 
 const filteredWords = arrayOfWords.filter((String) => String.length > 5);
 console.log("Liste an Wörtern: " + arrayOfWords)
 console.log("Wörter mit mehr als 5 Zeichen: " + filteredWords)
 
-// Step 3: Create an array of numbers. Use the '.reduce' function to calculate the sum of all the numbers in the array.
 
-// Das scheint mir zu kompliziert um richtig zu sein???
+
+
+// Step 3:
+// Create an array of numbers. Use the '.reduce' function to calculate the sum of all the numbers in the array.
+// ??? Das scheint mir zu kompliziert um richtig zu sein???
 
 
 const arrayOfNumbers: number [] = [2,4,6,8,10]
@@ -29,24 +37,45 @@ console.log("Liste an Zahlen: " + arrayOfNumbers)
 // ??? wie kann ich auf einzelne Elemente zugreifen? Mit charAt???
 console.log("Summe: " + sumNumbers)
 
-// Step 4: Create an array of numbers. Use the '.some' function to check if at least one number is greater than 10.
+
+
+
+// Step 4:
+// Create an array of numbers. Use the '.some' function to check if at least one number is greater than 10.
 
 const arrayOfNumbers2: number [] = [6,8,10,12,14]
 
 const biggerThenTen = (arrayOfNumbers2.some((number: number) => number > 10))
 if (biggerThenTen) {
     console.log(arrayOfNumbers2 + " hat einen Wert > 10");
-} else{
+} else {
     console.log(arrayOfNumbers2 + " hat keinen Wert > 10");
 }
 
-// Gegentest bei STtep 4
+// Gegentest zu Step 4
 
 const arrayOfNumbers3: number [] = [2,4,6,8]
 if (biggerThenTen) {
     console.log(arrayOfNumbers3 + " hat einen Wert > 10");
-} else{
+} else {
     console.log(arrayOfNumbers3 + " hat keinen Wert > 10");
 }
 
+
+//BONUS
+
+// Use only array functions for the following tasks.
+// Take this list of numbers:
+// const list = [15, 6, 3213, 9, 0, 12, 8464 , 1, 1264, 481, 186, 1031, 194];
+// Sort these numbers in descending order.
+// Square these numbers.
+// Remove the lowest two numbers and the highest 4 numbers from the list.
+// Remove all numbers that are divisible by 4.
+// Add up all the numbers.
+// What is the result?
+
+const longNumberList = [15, 6, 3213, 9, 0, 12, 8464 , 1, 1264, 481, 186, 1031, 194];
+console.log("Gegebene Liste: " + longNumberList)
+
+console.log("In ascending sortiert: " + longNumberList.sort())
 
